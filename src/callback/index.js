d9e0818e671ab53a78ba0612668b9ef558f65349
Oscar Terceros <osterce@gmail.com>
2021-03-31 22:44:01 -0400

    Multiples peticiones al API con XMLHttpRequest utilizando CallBack

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,23 +1,26 @@
+//suma dos valores
 function sum(num1, num2) {
   return num1 + num2;
 }
 
+//callback
 function calc(num1, num2, callback) {
-    return callback(num1, num2);
+  return callback(num1, num2);
 }
 
 console.log(calc(4, 8, sum));
 
 function date(callback) {
-  console.log(new Date);
-  setTimeout( function() {
+  console.log(new Date); //imprime la fecha actual
+  setTimeout(function () {
     let date = new Date;
     callback(date);
   }, 3000)
 }
 
+//callback
 function printDate(dateNow) {
-  console.log(dateNow);
+  console.log(`Imprime luego de 3 segundos ${dateNow}`);
 }
 
 date(printDate);
